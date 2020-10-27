c7a4ef7d05cd7bc2c5d284b07177db4ea2b1738d
Rafael Calunga <rafaelcalunga@gmail.com>
2020-10-27 18:02:13 -0400

    Creating NSManagedObject subclasses

---
+++ CoreDataProject/Movie+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Movie+CoreDataClass.swift
+//  CoreDataProject
+//
+//  Created by Rafael Calunga on 2020-10-27.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Movie)
+public class Movie: NSManagedObject {
+
+}
---
+++ CoreDataProject/Movie+CoreDataProperties.swift
@@ -0,0 +1,30 @@
+//
+//  Movie+CoreDataProperties.swift
+//  CoreDataProject
+//
+//  Created by Rafael Calunga on 2020-10-27.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension Movie {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Movie> {
+        return NSFetchRequest<Movie>(entityName: "Movie")
+    }
+
+    @NSManaged public var year: Int16
+    @NSManaged public var title: String
+    @NSManaged public var director: String
+    
+    public var wrappedTitle: String {
+        title ?? "Unknown Title"
+    }
+}
+
+extension Movie : Identifiable {
+
+}
